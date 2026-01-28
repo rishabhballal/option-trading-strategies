ee4f29a6d7e98f40ee90b820edb08048642aff18
Rishabh Ballal <rishabhballal@gmail.com>
2026-01-28 09:53:06 +0530

    refactor: import ots as package

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-import matplotlib.pyplot as plt
-import strats.bullish as bull
+import option_trading_strategies as ots
 
-strat = bull.covered_call
-
-strat.description()
-
-S_T = range(50, 151)
-plt.plot(S_T, [strat.payoff(x) for x in S_T])
-plt.plot(S_T, [0] * len(S_T), color='black')
-plt.xlabel('Final stock price')
-plt.ylabel('Payoff')
-plt.title(strat.title)
-plt.grid()
-plt.show()
+ots.bullish.covered_call.details()
