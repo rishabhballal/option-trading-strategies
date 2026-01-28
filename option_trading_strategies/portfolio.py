085dc308499d1ff10cf18b3273caa2b376b0881e
Rishabh Ballal <rishabhballal@gmail.com>
2026-01-28 12:34:34 +0530

    feat: add automatic S_T range estimation

--- option_trading_strategies/portfolio.py
+++ option_trading_strategies/portfolio.py
@@ -1,4 +1,3 @@
-import copy
 import matplotlib.pyplot as plt
 
 class Portfolio:
@@ -10,23 +9,21 @@ class Portfolio:
         return sum([x.payoff(S_T) for x in self.position['Long']]) - \
             sum([x.payoff(S_T) for x in self.position['Short']])
 
-    def details(self, S_T=range(201)):
+    def info(self):
         print(f'Strategy:\t{self.strategy}')
-
         pf = {'Long': [], 'Short': []}
         for k in self.position.keys():
             for x in self.position[k]:
                 if x.__class__.__name__ == 'Stock':
                     pf[k].append(x.__class__.__name__)
+                    S_0 = x.S_0
                 else:
                     pf[k].append(f'{x.__class__.__name__} struck at {x.strike}')
+                    S_0 = x.stock.S_0
         print(f'Portfolio:\t{pf}')
 
+        S_T = range(max(S_0 - 100, 0), S_0 + 101)
         P_T = [self._payoff(x) for x in S_T]
-        print(f'If the final stock price is between {min(S_T)} and {max(S_T)}:')
-        print(f'Max profit = {round(max(P_T))}', end='\t\t')
-        print(f'Max loss = {-round(min(P_T))}')
-
         plt.plot(S_T, P_T)
         plt.plot(S_T, [0] * len(S_T), color='black')
         plt.xlabel('Final stock price')
