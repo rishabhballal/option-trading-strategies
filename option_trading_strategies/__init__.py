c41b74befecbe5e671e9df4c88a99282828da48c
Rishabh Ballal <rishabhballal@gmail.com>
2026-01-29 11:03:58 +0530

    feat: create common stock instance for vanillas

--- option_trading_strategies/__init__.py
+++ option_trading_strategies/__init__.py
@@ -1,1 +1,11 @@
+from . import instruments
+
+stock = instruments.Stock(S_0=100, rate=0.05, vol=0.40)
+
+def call(strike=120, expiry=1):
+    return instruments.Call(stock=stock, strike=strike, expiry=expiry)
+
+def put(strike=80, expiry=1):
+    return instruments.Put(stock=stock, strike=strike, expiry=expiry)
+
 from .strats import bullish, bearish, volatility, sideways
