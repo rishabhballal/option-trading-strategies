10061d45a53f86424c97b48c3f72943128654d5a
Rishabh Ballal <rishabhballal@gmail.com>
2026-01-29 10:51:28 +0530

    feat: add protectives, bull spreads, synthetic forward

--- option_trading_strategies/strats/bullish.py
+++ option_trading_strategies/strats/bullish.py
@@ -1,11 +1,32 @@
-from .. import instruments
-from .. import portfolio
+from .. import stock, call, put
+from ..portfolio import Portfolio
 
-stock = instruments.Stock()
-call_120 = instruments.Call(stock=stock, strike=120)
-
-covered_call = portfolio.Portfolio(
+covered_call = Portfolio(
     strategy='Covered call',
     long=[stock],
-    short=[call_120]
+    short=[call(strike=120)]
+)
+
+protective_put = Portfolio(
+    strategy='Protective put',
+    long=[stock, put(strike=80)],
+    short=[]
+)
+
+bull_call_spread = Portfolio(
+    strategy='Bull call spread',
+    long=[call(strike=100)],
+    short=[call(strike=120)]
+)
+
+bull_put_spread = Portfolio(
+    strategy='Bull put spread',
+    long=[put(strike=80)],
+    short=[put(strike=100)]
+)
+
+long_synthetic_forward = Portfolio(
+    strategy='Long synthetic forward',
+    long=[call(strike=100)],
+    short=[put(strike=100)]
 )
