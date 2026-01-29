c3b6d6080f8c6bee3b185bfb83b086322943bab9
Rishabh Ballal <rishabhballal@gmail.com>
2026-01-29 18:55:48 +0530

    feat: add short synthetic straddles

--- option_trading_strategies/strats/sideways.py
+++ option_trading_strategies/strats/sideways.py
@@ -18,3 +18,15 @@ short_guts = Portfolio(
     long=[],
     short=[call(strike=80), put(strike=120)]
 )
+
+short_call_synthetic_straddle = Portfolio(
+    strategy='Short call synthetic straddle',
+    long=[stock],
+    short=[call(strike=100), call(strike=100)]
+)
+
+short_put_synthetic_straddle = Portfolio(
+    strategy='Short put synthetic straddle',
+    long=[],
+    short=[stock, put(strike=100), put(strike=100)]
+)
