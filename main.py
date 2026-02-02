49103e4a64340d229220b49bdbc4f048a3cd9c21
Rishabh Ballal <rishabhballal@gmail.com>
2026-02-02 21:47:32 +0530

    feat: add example of custom strategy

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import option_trading_strategies as ots
 
-ots.covered_call.info()
+# predefined
+ots.strats.long_iron_condor.plot_payoff()
+
+# custom
+ots.stock.reset(S_0=200, rate=0.04, vol=0.25)
+ots.Portfolio(
+    strategy='Long collar',
+    long=[ots.stock, ots.put(strike=180, expiry=2)],
+    short=[ots.call(strike=220, expiry=2)]
+).plot_payoff()
