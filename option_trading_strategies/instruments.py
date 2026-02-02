ae011e5485d5b74bf4f8f1b4900aacb874878697
Rishabh Ballal <rishabhballal@gmail.com>
2026-02-02 21:43:10 +0530

    refactor: add contents from portfolio.py

--- option_trading_strategies/instruments.py
+++ option_trading_strategies/instruments.py
@@ -1,5 +1,6 @@
 import math
 from scipy.stats import norm
+import matplotlib.pyplot as plt
 
 class Stock:
     def __init__(self, S_0, rate, vol):
@@ -9,6 +10,11 @@ class Stock:
 
     def payoff(self, S_T):
         return S_T - self.S_0
+
+    def reset(self, S_0, rate, vol):
+        self.S_0 = S_0
+        self.rate = rate
+        self.vol = vol
 
 class Call:
     def __init__(self, stock, strike, expiry):
@@ -43,3 +49,40 @@ class Put:
 
     def payoff(self, S_T):
         return max(self.strike - S_T, 0) - self.price()
+
+class Portfolio:
+    def __init__(self, strategy, long, short):
+        self.strategy = strategy
+        self.position = {'Long': long, 'Short': short}
+
+    def payoff(self, S_T):
+        return sum([x.payoff(S_T) for x in self.position['Long']]) - \
+            sum([x.payoff(S_T) for x in self.position['Short']])
+
+    def plot_payoff(self):
+        pf = {'Long': [], 'Short': []}
+        for k in self.position.keys():
+            for x in self.position[k]:
+                if x.__class__.__name__ == 'Stock':
+                    pf[k].append(x.__class__.__name__)
+                    S_0 = x.S_0
+                else:
+                    pf[k].append(f'{x.__class__.__name__} struck at {x.strike}')
+                    S_0 = x.stock.S_0
+        S_T = range(max(S_0 - 100, 0), S_0 + 101)
+        P_T = [self.payoff(x) for x in S_T]
+        plt.figure(figsize=(8, 6))
+        plt.plot(S_T, P_T)
+        plt.plot(S_T, [0] * len(S_T), color='black')
+        plt.xlabel('Final stock price')
+        plt.ylabel('Payoff')
+        plt.title(self.strategy)
+        plt.subplots_adjust(bottom=0.2)
+        plt.figtext(
+            0.125, 0.05,
+            f'Initial stock price = {S_0}\n' +
+            f'Long: [{', '.join(pf['Long'])}], ' +
+            f'Short: [{', '.join(pf['Short'])}]',
+            ha='left')
+        plt.grid()
+        plt.show()
