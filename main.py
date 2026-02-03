56f4d04b190968a39187dae6e9767f9762cfc6db
Rishabh Ballal <rishabhballal@gmail.com>
2026-02-03 16:42:19 +0530

    fix: instrument instances in custom strategy example

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import option_trading_strategies as ots
 ots.strats.long_iron_condor.plot_payoff()
 
 # custom
-ots.stock.reset(S_0=200, rate=0.04, vol=0.25)
+stock = ots.Stock(S_0=200, rate=0.04, vol=0.25)
 ots.Portfolio(
     strategy='Long collar',
-    long=[ots.stock, ots.put(strike=180, expiry=2)],
-    short=[ots.call(strike=220, expiry=2)]
+    long=[stock, ots.Put(stock=stock, strike=180, expiry=2)],
+    short=[ots.Call(stock=stock, strike=220, expiry=2)]
 ).plot_payoff()
